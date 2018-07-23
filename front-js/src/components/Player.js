161d10631ef7de4a98513088658790688e3599f8
Anton Sukhonosenko <algebraic@yandex-team.ru>
2018-07-23 23:07:10 +0300

    (you) in the players list

--- front-js/src/components/Player.js
+++ front-js/src/components/Player.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
 import Emoji from "./Emoji/Emoji";
+import authSelectors from "../store/authentication/selectors";
+import "./Player.css";
 
-export default function Player({ player }) {
+function Player({ player, isCurrentUser }) {
   // TODO: do not hardcode icon size
   return (
-    <span>
-      <Emoji type={player.iconType} size="22px" /> {player.user.name}
+    <span className="player">
+      <Emoji type={player.iconType} size="22px" /> {player.user.name}{" "}
+      {isCurrentUser && <span className="you">(you)</span>}
     </span>
   );
 }
+
+const mapStateToProps = (state, { player }) => ({
+  isCurrentUser: authSelectors.getCurrentUser(state).id === player.user.id
+});
+
+export default connect(mapStateToProps)(Player);
--- front-js/src/components/PlayersList.js
+++ front-js/src/components/PlayersList.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
+import { sortBy } from "lodash";
 import selectors from "../store/boards/selectors";
 import Player from "./Player";
+import authSelectors from "../store/authentication/selectors";
 
 function PlayersList({ players }) {
   return (
@@ -17,8 +19,11 @@ function PlayersList({ players }) {
 
 const mapStateToProps = (state, { boardId }) => {
   const board = selectors.getBoardById(state, boardId);
+  const currentUser = authSelectors.getCurrentUser(state);
+  const isCurrentUser = player => player.user.id === currentUser.id;
+
   return {
-    players: board.players
+    players: sortBy(board.players, [p => (isCurrentUser(p) ? 0 : 1)])
   };
 };
 
