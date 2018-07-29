6a723bf604116dbb4c93803629eddc32fc982ee8
Anton Sukhonosenko <algebraic@yandex-team.ru>
2018-07-30 00:46:18 +0300

    indexed token field

--- backend/src/main/kotlin/com/example/tictactoe/auth/User.kt
+++ backend/src/main/kotlin/com/example/tictactoe/auth/User.kt
@@ -1,7 +1,12 @@
 package com.example.tictactoe.auth
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "users")
-data class User(@Id val id: String, val name: String, val token: String)
+data class User(
+    @Id val id: String,
+    val name: String,
+    @Indexed val token: String
+)
