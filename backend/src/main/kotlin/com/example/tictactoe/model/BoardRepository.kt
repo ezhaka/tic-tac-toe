d3ab2e9d3f3b1ea9fc698745c00d1c5b875b7500
Anton Sukhonosenko <algebraic@yandex-team.ru>
2018-07-30 01:04:58 +0300

    proper lastAssignedId counter initialization

--- backend/src/main/kotlin/com/example/tictactoe/model/BoardRepository.kt
+++ backend/src/main/kotlin/com/example/tictactoe/model/BoardRepository.kt
@@ -4,9 +4,12 @@ import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Repository
 interface BoardRepository : ReactiveMongoRepository<Board, Int> {
     @Query("{ 'winner': null }")
     fun findAllActive(): Flux<Board>
+
+    fun findFirstByOrderByIdDesc(): Mono<Board>
 }
--- backend/src/main/kotlin/com/example/tictactoe/model/BoardService.kt
+++ backend/src/main/kotlin/com/example/tictactoe/model/BoardService.kt
@@ -1,12 +1,18 @@
 package com.example.tictactoe.model
 
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import java.util.concurrent.atomic.AtomicInteger
 
 @Service
-class BoardService(private val repository: BoardRepository) {
+class BoardService(private val repository: BoardRepository) : InitializingBean {
     private val lastAssignedId: AtomicInteger = AtomicInteger()
+
+    override fun afterPropertiesSet() {
+        val lastBoard = repository.findFirstByOrderByIdDesc().block()
+        lastAssignedId.set(lastBoard?.id ?: 0)
+    }
 
     fun getById(boardId: Int) = repository.findById(boardId)
 
